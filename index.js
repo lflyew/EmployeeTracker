d6a0b816f05d51fb53a0b5f842a97a40e90d48c8
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-12-02 01:13:51 -0500

    packages installed, index file and seeds file started

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+//require packages
+
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 const consoleTable = require ('console.table');
 
-
+//db connect
 const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'root',
-        database: "company_db"
+        password: 'password',
+        database: "companyemp_db"
     },
-    console.log('Connected to database.')
+    console.log('Connected to companyemp database.')
 
 );
